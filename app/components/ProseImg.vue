64ebdc15ffc7188f99f3ac0f0078fac41a4bdf03
tungchiahui <tungchiahui@gmail.com>
2025-12-27 23:45:22 +0800

    add lazy

---
+++ app/components/ProseImg.vue
@@ -0,0 +1,33 @@
+<template>
+  <img 
+    :src="src" 
+    :alt="alt" 
+    loading="lazy" 
+    decoding="async"
+    class="prose-img"
+  />
+</template>
+
+<script setup lang="ts">
+// 定义从 Markdown 传入的属性
+defineProps({
+  src: {
+    type: String,
+    default: ''
+  },
+  alt: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<style scoped>
+.prose-img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+  margin: 1.5rem auto;
+  border-radius: 8px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
+  modules: ['@nuxt/content', '@nuxt/image'],
+  image: {
+    // 因为你的图片在 cdn.tungchiahui.cn，建议加上域名许可
+    domains: ['cdn.tungchiahui.cn']
+  },
   // 告诉 Nuxt 使用第 4 版的规范
   future: {
     compatibilityVersion: 4,
