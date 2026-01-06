99b1bde8d13738c2ba3da4875ebfd27ce222b788
tungchiahui <tungchiahui@gmail.com>
2026-01-06 17:06:12 +0800

    update https://music.3e0.cn/

--- app/components/MusicPlayer.vue
+++ app/components/MusicPlayer.vue
@@ -10,7 +10,9 @@
       type="playlist"
       id="9619599108"
       autoplay="false"
+      api="https://music.3e0.cn/?server=:server&type=:type&id=:id&r=:r"
     />
+    <!-- api="https://api.injahow.cn/meting/?server=:server&type=:type&id=:id&r=:r" -->
   </div>
 
   <!-- 🎧 收起 / 展开按钮 -->
