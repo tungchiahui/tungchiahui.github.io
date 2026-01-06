2390ae8d884130373332806566f6cb78f3fcb114
tungchiahui <tungchiahui@gmail.com>
2026-01-06 14:04:51 +0800

    add wiki2.0

--- app/components/Assets.vue
+++ app/components/Assets.vue
@@ -3,7 +3,9 @@
 // 这个组件不需要任何逻辑
 </script>
 
+<style src="~/assets/css/index.css"></style>
 <style src="~/assets/css/blogwiki.css"></style>
+
 
 <style lang="scss">
 @use "~/assets/scss/index.scss" as *;
