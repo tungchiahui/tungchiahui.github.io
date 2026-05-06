b0a884a564e01e6a7caa569d300ef1a345d4e6bb
tungchiahui <tungchiahui@gmail.com>
2026-05-06 23:44:45 +0800

    update musicplayer

--- app/components/MusicPlayer.vue
+++ app/components/MusicPlayer.vue
@@ -19,8 +19,20 @@
     :class="{ open: !hidden }"
     @click="toggle"
     :aria-pressed="!hidden"
+    :aria-label="hidden ? '展开音乐播放器' : '收起音乐播放器'"
+    :title="hidden ? '展开音乐播放器' : '收起音乐播放器'"
   >
-    {{ hidden ? '🎧 展开' : '🎧 收起' }}
+    <span class="toggle-player-icon" aria-hidden="true">
+      <svg viewBox="0 0 24 24">
+        <path d="M9 18V5l11-2v13" />
+        <path d="M9 18a3 3 0 1 1-3-3 3 3 0 0 1 3 3Z" />
+        <path d="M20 16a3 3 0 1 1-3-3 3 3 0 0 1 3 3Z" />
+      </svg>
+    </span>
+    <span class="toggle-player-text">{{ hidden ? '展开' : '收起' }}</span>
+    <svg class="toggle-player-chevron" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="m6 9 6 6 6-6" />
+    </svg>
   </button>
 </template>
 
